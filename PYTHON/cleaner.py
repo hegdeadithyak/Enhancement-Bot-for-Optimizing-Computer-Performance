971ebe7c242571a14f98709958377d63d75d8b3f
Adithya4720 <hegdeadithyak@gmail.com>
2023-09-27 14:53:17 +0530

    Bot using python

---
+++ PYTHON/cleaner.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+#getting username
+print("Just a second :),Hope you don't mind.")
+def username():
+    for name in ('LOGNAME', 'USER', 'LNAME', 'USERNAME'):
+       user= os.environ.get(name)
+       if user:
+          return user
+       
+
+def clear_tempfile(user):
+   path = r"C:\Users\\" +user+"\AppData\Local\Temp"
+   contents= os.listdir(path)
+   for item in contents:
+      item_path=os.path.join(path,item)
+      try:
+         if os.path.isfile(item_path):
+            os.remove(item_path)
+         elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+         else:
+                print(f"Skipped: {item_path} (Not a file or directory)\n")
+      except Exception as e:
+            print(f"Error deleting {item_path}: {e}\n")
+
+          
+if __name__ ==  "__main__":
+    user=username()
+    clear_tempfile(user)
+
+    print("Cleaned Successfully")
+   
+
