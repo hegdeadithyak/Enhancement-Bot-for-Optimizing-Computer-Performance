b53cc78070eeb13c06e49fc7c9aab9501d4bc8de
Adithya4720 <hegdeadithyak@gmail.com>
2023-10-02 21:16:52 +0530

    Added Prefetch Still .exe file needs to be updated

--- PYTHON/cleaner.py
+++ PYTHON/cleaner.py
@@ -9,7 +9,7 @@ def username():
        if user:
           return user
        
-
+#clearing tempfiles
 def clear_tempfile(user):
    path = r"C:\Users\\" +user+"\AppData\Local\Temp"
    contents= os.listdir(path)
@@ -25,7 +25,23 @@ def clear_tempfile(user):
       except Exception as e:
             print(f"Error deleting {item_path}: {e}\n")
 
-          
+#clearing prefetch
+def clear_prefetch(user):
+   path = r"C:\Windows\Prefetch"
+   contents= os.listdir(path)
+   for item in contents:
+      item_path=os.path.join(path,item)
+      try:
+         if os.path.isfile(item_path):
+            os.remove(item_path)
+         elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+         else:
+                print(f"Skipped: {item_path} (Not a file or directory)\n")
+      except Exception as e:
+            print(f"Error deleting {item_path}: {e}\n")
+
+#clearing recent
 if __name__ ==  "__main__":
     user=username()
     clear_tempfile(user)
